0eff1296fbe001bde16d4d1cdcad194a572ab429
Yonghua <yonghua.zheng@esimtek.com>
2016-11-15 12:23:03 +0800

    [BACKEND][LAYOUT] Refine layout
    Refine the global layout for the content.
    
    Change-Id: I85caa90f5b0fdf19b2102dbb8439ba145e74db57

--- backend/views/layouts/content.php
+++ backend/views/layouts/content.php
@@ -5,33 +5,16 @@ use dmstr\widgets\Alert;
 ?>
 <div class="content-wrapper">
     <section class="content-header">
-        <?php if (isset($this->blocks['content-header'])) { ?>
-            <h1><?= $this->blocks['content-header'] ?></h1>
-        <?php } else { ?>
-            <h1>
-                <?php
-                if ($this->title !== null) {
-                    echo \yii\helpers\Html::encode($this->title);
-                } else {
-                    echo \yii\helpers\Inflector::camel2words(
-                        \yii\helpers\Inflector::id2camel($this->context->module->id)
-                    );
-                    echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
-                } ?>
-            </h1>
-        <?php } ?>
-
-        <?=
-        Breadcrumbs::widget(
-            [
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]
-        ) ?>
+        <?= Breadcrumbs::widget(
+                [
+                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                ]
+            ) ?>
     </section>
 
     <section class="content">
-        <?= Alert::widget() ?>
-        <?= $content ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
     </section>
 </div>
 
@@ -39,6 +22,5 @@ use dmstr\widgets\Alert;
     <div class="pull-right hidden-xs">
         <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2016 <a href="http://www.esimtek.com">Esimtek</a>.</strong> All rights
-    reserved.
+        <strong>Copyright &copy; 2016 <a href="http://www.esimtek.com">Esimtek</a>.</strong> All rights reserved.
 </footer>
